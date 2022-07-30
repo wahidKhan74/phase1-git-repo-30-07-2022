3e4c2b9bac1799f6e6d9e72c6295d5572b8e2447
Wahid Khan <wahid.khan74@gmail.com>
2022-07-30 15:52:09 +0000

    added footer

--- src/App.js
+++ src/App.js
@@ -1,1 +1,6 @@
 console.log("This is a simple JavaScript File");
+console.log("This is a simple JavaScript File");
+console.log("This is a simple JavaScript File");
+console.log("This is a simple JavaScript File");
+console.log("This is a simple JavaScript File");
+console.log("This is a simple JavaScript File");
